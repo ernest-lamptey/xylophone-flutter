a0176433b21567bffcc89f556d56ef0a519976e5
ernest-tt <122813064+ernest-tt@users.noreply.github.com>
2023-01-28 18:20:59 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
